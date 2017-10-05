709ee4d271202b481fee5bfb7219619295d16d16
Boniface Mwenda <boniface.mwenda@andela.com>
2017-10-05 12:16:53 +0300

    Implement crud for users

--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,4 @@ Rails.application.routes.draw do
   get 'users/update'
 
   get 'users/destroy'
-
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
