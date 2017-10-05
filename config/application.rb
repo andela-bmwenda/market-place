23ba6b98daedeb32ec8d9b87830d56314c51bca3
Boniface Mwenda <boniface.mwenda@andela.com>
2017-10-05 12:00:14 +0300

    Make app api only

--- config/application.rb
+++ config/application.rb
@@ -9,17 +9,20 @@ require "action_controller/railtie"
 # require "action_mailer/railtie"
 require "action_view/railtie"
 require "action_cable/engine"
-require "sprockets/railtie"
+# require "sprockets/railtie"
 # require "rails/test_unit/railtie"
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
-module MarketPlaceApi
+module MapItApi
   class Application < Rails::Application
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
+
+    # Make app api only
+    config.api_only = true
   end
 end
