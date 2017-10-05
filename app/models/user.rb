5e663a570df7db4efbaf35602c4d0751fbb6e553
Boniface Mwenda <boniface.mwenda@andela.com>
2017-10-05 20:07:42 +0300

    Add model specs

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,7 @@
 class User < ApplicationRecord
+  has_secure_password
+
+  validates :first_name, :last_name, :email, :password_digest, presence: true
+  validates :email, uniqueness: true
+  validates_format_of :email, with: /\A[^@]+@([^@\.]+\.)+[^@\.]+\z/
 end
